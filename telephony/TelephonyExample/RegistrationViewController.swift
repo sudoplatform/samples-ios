d13ec2d0dc7782d60ba4068010e2230e2b664635
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2020-05-28 13:14:21 +1000

    Release TelephonyExample 2.1.0
    ## 2.1.0
    
    Released May 28, 2020
    
    ### New
    
    - Added example code to demonstrate use of federated sign in. If sudoplatformconfig.json file contains registration method "FSSO" then federated sign in will be used.

--- telephony/TelephonyExample/RegistrationViewController.swift
+++ telephony/TelephonyExample/RegistrationViewController.swift
@@ -63,31 +63,56 @@ class RegistrationViewController: UIViewController {
             }
         }
 
-        if userClient.isRegistered() {
-            signIn()
+        if userClient.getSupportedRegistrationChallengeType().contains(.fsso) {
+            guard let navigationController = self.navigationController else {
+                return completion(false)
+            }
+
+            do {
+                try userClient.presentFederatedSignInUI(navigationController: navigationController) { signInResult in
+                    switch signInResult {
+                    case .failure(let error):
+                        self.showSignInFailureAlert(error: error)
+                        completion(false)
+                    case .success:
+                        completion(true)
+                    }
+                }
+            } catch let signInError {
+                self.showSignInFailureAlert(error: signInError)
+                completion(false)
+            }
         } else {
-            authenticator.register { registerResult in
-                switch registerResult {
-                case .failure(let error):
-                    self.showRegistrationFailureAlert(error: error)
-                    completion(false)
-                case .success:
-                    signIn()
+            if userClient.isRegistered() {
+                signIn()
+            } else {
+                authenticator.register { registerResult in
+                    switch registerResult {
+                    case .failure(let error):
+                        self.showRegistrationFailureAlert(error: error)
+                        completion(false)
+                    case .success:
+                        signIn()
+                    }
                 }
             }
         }
     }
 
     private func showRegistrationFailureAlert(error: Error) {
-        let alert = UIAlertController(title: "Error", message: "Failed to register:\n\(error.localizedDescription)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: "Failed to register:\n\(error.localizedDescription)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     private func showSignInFailureAlert(error: Error) {
-        let alert = UIAlertController(title: "Error", message: "Failed to sign in:\n\(error.localizedDescription)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: "Failed to sign in:\n\(error.localizedDescription)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     private func navigateToSudoList() {
